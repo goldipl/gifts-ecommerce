4e00c54212f0ddb4edabb0edd4c59bbf38704405
goldipl <mgodlewski92@gmail.com>
2025-02-18 07:36:31 +0100

    beautify html

--- components/common/topbar.php
+++ components/common/topbar.php
@@ -1,20 +1,17 @@
 <div id="topbar">
     <div class="container d-flex justify-content-between align-items-center">
         <div class="d-flex flex-row left-topbar">
-                <a class="topbar-link" href="./index.php#about_us" class="p-3">
-                    O nas
-                </a>
-                <a class="topbar-link" href="" class="p-3">
-                    Ulubione
-                </a>
-                <p class="topbar-text">
-                Godziny otwarcia: pon-pią: <span>08:00-17:00</span>
-</p>
+            <a class="topbar-link" href="./index.php#about_us" class="p-3"> O nas </a>
+            <a class="topbar-link" href="" class="p-3"> Ulubione </a>
+            <p class="topbar-text"> Godziny otwarcia: pon-pią: <span>08:00-17:00</span>
+            </p>
         </div>
         <div class="d-flex">
             <div class="topbar-links d-flex flex-row align-items-center">
-                <a class="gtm-mail" href="mailto:biuro@gifts24.pl"><img width="17" height="14" src="./assets/icons/common/topbar/topbar-envelope.svg" alt="e-mail">biuro@gifts24.pl</a>
-                <a class="gtm-phone" href="tel:+48505178262"><img width="15" height="15" src="./assets/icons/common/topbar/topbar-telephone.svg" alt="telefon">+48 505 178 262</a>
+                <a class="gtm-mail" href="mailto:biuro@gifts24.pl">
+                    <img width="17" height="14" src="./assets/icons/common/topbar/topbar-envelope.svg" alt="e-mail">biuro@gifts24.pl </a>
+                <a class="gtm-phone" href="tel:+48505178262">
+                    <img width="15" height="15" src="./assets/icons/common/topbar/topbar-telephone.svg" alt="telefon">+48 505 178 262 </a>
             </div>
         </div>
     </div>
