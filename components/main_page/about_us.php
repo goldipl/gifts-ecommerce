6856ae4eea711943ac9930984f482bb5d2320ac7
goldipl <mgodlewski92@gmail.com>
2025-02-18 12:48:45 +0100

    styling about us section

--- components/main_page/about_us.php
+++ components/main_page/about_us.php
@@ -14,7 +14,7 @@
     <div class="container article-text-sg">
         <div class="d-flex flex-row article-text-sg__content">
             <div class="d-flex w-50 me-4 flex-column left-column">
-                <h3><span>GIFTS24 - </span>gadżety to nie wszystko!</h3>
+                <h3 class="thin"><span>GIFTS24 - </span>gadżety to nie wszystko!</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Vivamus dictum turpis id diam accumsan, ut rhoncus purusviverra. Donec velit elit, euismod eget diam quis, eleifendtincidunt arcu. Phasellus vitae elementum urna, a molestieerat.</p>
                 <p>Proin scelerisque lobortis vehicula. Ut non lacus ut diamrhoncus hendrerit a eget arcu. Donec elementum vestibulumdiam, at laoreet ipsum maximus ut. Morbi efficitur nunc atligula dapibus, feugiat ultrices lorem vulputate. Nullamgravida risus nibh, vitae convallis diam pulvinar eget. Nullamid tortor arcu.</p>
             </div>
