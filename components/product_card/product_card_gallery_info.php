e21bdc7292f048459d82e4394e805c979233f8cc
goldipl <mgodlewski92@gmail.com>
2025-02-19 19:05:39 +0100

    fill and styling products card

--- components/product_card/product_card_gallery_info.php
+++ components/product_card/product_card_gallery_info.php
@@ -1,196 +1,209 @@
 <div class="product_card_gallery_info container">
-    <div class="product-gallery">
-        <div class="container">
-            <!-- top gallery -->
-            <div class="swiper gallery article-gallery slider-one">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="swiper-zoom-container">
-                            <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
-                        </div>
-                        <!-- Product icons -->
-                        <div class="product-icons">
-                            <span class="product-zoom-in">
-                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
-                            </span>
-                            <a class="product-download" href="#">
-                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="swiper-zoom-container">
-                            <img class="zoom-image" src="./assets/img/product_card/product-card-02.jpg" href="./assets/img/product_card/product-card-02.jpg">
-                        </div>
-                        <!-- Product icons -->
-                        <div class="product-icons">
-                            <span class="product-zoom-in">
-                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
-                            </span>
-                            <a class="product-download" href="#">
-                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="swiper-zoom-container">
-                            <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
-                        </div>
-                        <!-- Product icons -->
-                        <div class="product-icons">
-                            <span class="product-zoom-in">
-                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
-                            </span>
-                            <a class="product-download" href="#">
-                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="swiper-zoom-container">
-                            <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
-                        </div>
-                        <!-- Product icons -->
-                        <div class="product-icons">
-                            <span class="product-zoom-in">
-                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
-                            </span>
-                            <a class="product-download" href="#">
-                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="swiper-zoom-container">
-                            <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
-                        </div>
-                        <!-- Product icons -->
-                        <div class="product-icons">
-                            <span class="product-zoom-in">
-                                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
-                            </span>
-                            <a class="product-download" href="#">
-                                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Product label -->
-                <div class="product-label-box">
-                    <div class="product-label" style="background-color: #E4EDFF;">
-                        <p>Promocja</p>
-                    </div>
-                </div>
+  <div class="product-gallery">
+    <div class="container">
+      <!-- top gallery -->
+      <div class="swiper gallery article-gallery slider-one">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="swiper-zoom-container">
+              <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
             </div>
-            <!-- thumbs -->
-            <div class="thumbs-overflow-wrapper">
-                <div class="swiper gallery-thumbs slider-two">
-                    <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <img src="./assets/img/product_card/product-card-01.jpg">
-                        </div>
-                        <div class="swiper-slide">
-                            <img src="./assets/img/product_card/product-card-02.jpg">
-                        </div>
-                        <div class="swiper-slide">
-                            <img src="./assets/img/product_card/product-card-01.jpg">
-                        </div>
-                        <div class="swiper-slide">
-                            <img src="./assets/img/product_card/product-card-01.jpg">
-                        </div>
-                        <div class="swiper-slide">
-                            <img src="./assets/img/product_card/product-card-01.jpg">
-                        </div>
-                    </div>
-                </div>
-                <!-- Add Arrows -->
-                <div title="next" class="swiper-button-next"></div>
-                <div title="prev" class="swiper-button-prev"></div>
+            <!-- Product icons -->
+            <div class="product-icons">
+              <span class="product-zoom-in">
+                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+              </span>
+              <a class="product-download" href="#">
+                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+              </a>
             </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-zoom-container">
+              <img class="zoom-image" src="./assets/img/product_card/product-card-02.jpg" href="./assets/img/product_card/product-card-02.jpg">
+            </div>
+            <!-- Product icons -->
+            <div class="product-icons">
+              <span class="product-zoom-in">
+                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+              </span>
+              <a class="product-download" href="#">
+                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+              </a>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-zoom-container">
+              <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
+            </div>
+            <!-- Product icons -->
+            <div class="product-icons">
+              <span class="product-zoom-in">
+                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+              </span>
+              <a class="product-download" href="#">
+                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+              </a>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-zoom-container">
+              <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
+            </div>
+            <!-- Product icons -->
+            <div class="product-icons">
+              <span class="product-zoom-in">
+                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+              </span>
+              <a class="product-download" href="#">
+                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+              </a>
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="swiper-zoom-container">
+              <img class="zoom-image" src="./assets/img/product_card/product-card-01.jpg" href="./assets/img/product_card/product-card-01.jpg">
+            </div>
+            <!-- Product icons -->
+            <div class="product-icons">
+              <span class="product-zoom-in">
+                <img class="zoom-icon" src="./assets/icons/product_page/zoom.svg" alt="zoom icon">
+              </span>
+              <a class="product-download" href="#">
+                <img class="download-icon" src="./assets/icons/product_page/download.svg" alt="download icon">
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- Product label -->
+        <div class="product-label-box">
+          <div class="product-label" style="background-color: #E4EDFF;">
+            <p>Promocja</p>
+          </div>
+        </div>
+      </div>
+      <!-- thumbs -->
+      <div class="thumbs-overflow-wrapper">
+        <div class="swiper gallery-thumbs slider-two">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <img src="./assets/img/product_card/product-card-01.jpg">
+            </div>
+            <div class="swiper-slide">
+              <img src="./assets/img/product_card/product-card-02.jpg">
+            </div>
+            <div class="swiper-slide">
+              <img src="./assets/img/product_card/product-card-01.jpg">
+            </div>
+            <div class="swiper-slide">
+              <img src="./assets/img/product_card/product-card-01.jpg">
+            </div>
+            <div class="swiper-slide">
+              <img src="./assets/img/product_card/product-card-01.jpg">
+            </div>
+          </div>
+        </div>
+        <!-- Add Arrows -->
+        <div title="next" class="swiper-button-next"></div>
+        <div title="prev" class="swiper-button-prev"></div>
+      </div>
+    </div>
+  </div>
+  <div class="product-info">
+    <div class="product-info_title">
+      <h1>Długopis metalowy</h1>
+    </div>
+    <div class="product-info_short-description">
+      <div class="product-info_code">Kod produktu KC2585-40</div>
+    </div>
+    <div class="product-info_quantity">
+      <div class="info_quantity_text">
+        <p>Liczba dostępnych sztuk:</p>
+      </div>
+      <div class="info_quantity_box">
+        <p>128</p>
+      </div>
+      <div class="info_quantity_box">
+        <p>Polska: <span>128</span>
+        </p>
+      </div>
+    </div>
+    <div class="colors-availability-wrapper">
+      <p class="product-info_availability-colors-title">Dostępne kolory:</p>
+      <div class="product-info_availability-colors">
+        <a href="#" class="color grey"></a>
+        <a href="#" class="color red"></a>
+        <a href="#" class="color yellow"></a>
+        <a href="#" class="color lightblue"></a>
+        <a href="#" class="color blue"></a>
+        <a href="#" class="color pink"></a>
+        <a href="#" class="color purple"></a>
+        <a href="#" class="color green"></a>
+        <a href="#" class="color black checked"></a>
+      </div>
+    </div>
+    <div class="brutto-netto-box">
+      <label class="custom-radio-container netto">Cena netto <input type="radio" name="price-radio" class="custom_input" checked>
+        <span class="custom-radio-checkmark"></span>
+      </label>
+      <label class="custom-radio-container brutto">Cena brutto <input type="radio" name="price-radio" class="custom_input">
+        <span class="custom-radio-checkmark"></span>
+      </label>
+    </div>
+    <div class="price-value-box">
+        <p>Cena od: <span>1,66 PLN</span></p>
+    </div>
+    <div class="price-input">
+        <button class="minus-btn" onclick="decrease()">
+            <img src="./assets/icons/product_page/minus-circle.svg" alt="minus" width="26" height="26">
+        </button>
+        <input type="text" id="price-value" value="0" min="0">
+        <button class="plus-btn" onclick="increase()">
+            <img src="./assets/icons/product_page/plus-circle.svg" alt="plus" width="26" height="26">
+        </button>
+        <button class="add-to-cart">
+            <span>Do Koszyka</span>
+            <img src="./assets/icons/product_page/cart.svg" alt="koszyk" width="26" height="26">
+        </button>
+    </div>
+    <div class="branding-custom-select">
+        <div class="select-box" onclick="toggleDropdown()">
+            <span id="selected-option">Dodaj znakowanie:</span>
+            <span class="arrow"></span>
+        </div>
+        <div class="options">
+            <div class="option" onclick="selectOption('Sitodruk')">Sitodruk</div>
+            <div class="option" onclick="selectOption('Druk cyfrowy')">Druk cyfrowy</div>
+            <div class="option" onclick="selectOption('Termotransfer')">Termotransfer</div>
         </div>
     </div>
-    <div class="product-info">
-        <div class="product-info_title">
-            <h1>Długopis metalowy</h1>
-        </div>
-        <div class="product-info_short-description">
-            <div class="product-info_code">Kod: <span>1333903</span>
-            </div>
-            <div class="product-info_color">Kolor: <span>czarny</span>
-            </div>
-            <div class="product-info_ean">EAN: <span>4064571004023</span>
-            </div>
-        </div>
-        <p class="product-info_availability-colors-title">Dostępne kolory:</p>
-        <div class="product-info_availability-colors">
-            <a href="#" class="color grey"></a>
-            <a href="#" class="color red"></a>
-            <a href="#" class="color yellow"></a>
-            <a href="#" class="color lightblue"></a>
-            <a href="#" class="color blue"></a>
-            <a href="#" class="color pink"></a>
-            <a href="#" class="color purple"></a>
-            <a href="#" class="color green"></a>
-            <a href="#" class="color black checked"></a>
-        </div>
-        <div class="product-price-box">
-            <div class="product-price-box_top">
-                <div class="product-price-box_top__left">
-                    <div class="your-price">
-                        <span>Cena: <p>2,55 PLN</p>
-                        </span>
-                    </div>
-                </div>
-                <a href="" class="product-price-box_top__right">
-                    <img src="./assets/icons/product_page/calculator.svg" alt="calc icon">
-                    <p>Kalkulator</p>
-                </a>
-            </div>
-            <div class="product-price-box_bottom">
-                <div class="product-price-box_bottom__left">
-                    <span>Cena promocyjna <p>1,88 PLN</p>
-                    </span>
-                    <p class="minimal-quantity">Minimalna ilość: 500 szt.</p>
-                </div>
-                <button class="product-price-box_bottom__right">
-                    <img src="./assets/icons/product_page/cart.svg" alt="cart icon">
-                    <p>Dodaj do koszyka</p>
-                </button>
-            </div>
-        </div>
-        <div class="product-more-box">
-            <div class="product-more-box__row">
-                <a href="#" class="product-more-box__col">
-                    <img src="./assets/icons/product_page/nego-price.svg" alt="download icon">
-                    <p>Negocjuj cenę</p>
-                </a>
-                <a href="#" class="product-more-box__col">
-                    <img src="./assets/icons/product_page/comparison.svg" alt="comparision icon">
-                    <p>Porównywarka</p>
-                </a>
-            </div>
-            <div class="product-more-box__row">
-                <a href="#" class="product-more-box__col">
-                    <img src="./assets/icons/product_page/offert.svg" alt="download icon">
-                    <p>Ofertownik</p>
-                </a>
-                <a href="#" class="product-more-box__col">
-                    <img src="./assets/icons/product_page/download.svg" alt="comparision icon">
-                    <p>Pobierz ulotkę PDF</p>
-                </a>
-            </div>
-            <div class="product-more-box__row last-row">
-                <a href="#" class="product-more-box__col">
-                    <img src="./assets/icons/product_page/branding-time.svg" alt="download icon">
-                    <p>Znakowanie: <span>2 dni</span>
-                    </p>
-                </a>
-                <a href="#" class="product-more-box__col">
-                    <img src="./assets/icons/product_page/branding-location.svg" alt="comparision icon">
-                    <p>Miejsce znakowania</p>
-                </a>
-            </div>
-        </div>
-    </div>
+  </div>
 </div>
 
+<script>
+        function increase() {
+            let input = document.getElementById('price-value');
+            input.value = Math.max(0, parseInt(input.value) + 1);
+        }
+
+        function decrease() {
+            let input = document.getElementById('price-value');
+            input.value = Math.max(0, parseInt(input.value) - 1);
+        }
+
+        function toggleDropdown() {
+            document.querySelector('.branding-custom-select').classList.toggle('open');
+        }
+
+        function selectOption(value) {
+            document.getElementById('selected-option').innerText = value;
+            toggleDropdown();
+        }
+
+        document.addEventListener('click', function(event) {
+            let dropdown = document.querySelector('.branding-custom-select');
+            if (!dropdown.contains(event.target)) {
+                dropdown.classList.remove('open');
+            }
+        });
+</script>
