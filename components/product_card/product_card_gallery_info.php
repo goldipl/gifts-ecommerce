7158b1e6181ba358d73c9be05e75a5f8c82a9ee4
goldipl <mgodlewski92@gmail.com>
2025-02-19 20:24:07 +0100

    fill and styling products card branding products options

--- components/product_card/product_card_gallery_info.php
+++ components/product_card/product_card_gallery_info.php
@@ -151,61 +151,130 @@
       </label>
     </div>
     <div class="price-value-box">
-        <p>Cena od: <span>1,66 PLN</span></p>
+      <p>Cena od: <span>1,66 PLN</span>
+      </p>
     </div>
     <div class="price-input">
-        <div class="price-value-btn-box">
-            <button class="minus-btn" onclick="decrease()">
-                <img src="./assets/icons/product_page/minus-circle.svg" alt="minus" width="26" height="26">
-            </button>
-            <input type="text" id="price-value" value="0" min="0">
-            <button class="plus-btn" onclick="increase()">
-                <img src="./assets/icons/product_page/plus-circle.svg" alt="plus" width="26" height="26">
-            </button>
-        </div>
-        <button class="add-to-cart">
-            <span>Do Koszyka</span>
-            <img src="./assets/icons/product_page/cart.svg" alt="koszyk" width="26" height="26">
+      <div class="price-value-btn-box">
+        <button class="minus-btn" onclick="decrease()">
+          <img src="./assets/icons/product_page/minus-circle.svg" alt="minus" width="26" height="26">
         </button>
+        <input type="number" id="price-value" value="0" min="0">
+        <button class="plus-btn" onclick="increase()">
+          <img src="./assets/icons/product_page/plus-circle.svg" alt="plus" width="26" height="26">
+        </button>
+      </div>
+      <button class="add-to-cart">
+        <span>Do Koszyka</span>
+        <img src="./assets/icons/product_page/cart.svg" alt="koszyk" width="26" height="26">
+      </button>
     </div>
     <div class="branding-custom-select">
-        <div class="select-box" onclick="toggleDropdown()">
-            <span id="selected-option">Dodaj znakowanie:</span>
-            <span class="arrow"></span>
+      <div class="select-box" onclick="toggleDropdown()">
+        <span id="selected-option">Dodaj znakowanie:</span>
+        <span class="arrow"></span>
+      </div>
+      <div class="branding-product-options">
+        <table>
+          <tr>
+            <td>Znakowanie</td>
+            <td>
+              <select name="branding-type" id="branding-type" class="form-select">
+                <option value="Sitodruk">Sitodruk</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>Ilość produktów</td>
+            <td>
+              <div class="counter">
+                <button onclick="changeValue('products', -1)">
+                  <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.43031 11.93C6.54031 11.88 6.64031 11.82 6.74031 11.73C6.83031 11.64 6.89031 11.54 6.94031 11.43C7.03031 11.21 7.03031 10.95 6.94031 10.72C6.89031 10.61 6.83031 10.51 6.74031 10.42L2.25031 6L6.73031 1.58C6.82031 1.49 6.88031 1.39 6.93031 1.28C7.02031 1.05 7.02031 0.79 6.93031 0.57C6.88031 0.46 6.81031 0.36 6.73031 0.27C6.64031 0.19 6.54031 0.12 6.42031 0.07C6.31031 0.02 6.19031 9.74859e-09 6.07031 1.11796e-08C5.95031 1.26106e-08 5.83031 0.02 5.72031 0.07C5.61031 0.12 5.51031 0.18 5.42031 0.27L0.270312 5.35C0.180312 5.44 0.120313 5.54 0.0703126 5.65C0.0203124 5.76 0.000312398 5.88 0.0003124 6C0.000312401 6.12 0.0203124 6.24 0.0703126 6.36C0.120313 6.47 0.180312 6.57 0.270312 6.66L5.42031 11.73C5.51031 11.81 5.61031 11.88 5.72031 11.93C5.83031 11.97 5.95031 12 6.07031 12C6.19031 12 6.31031 11.98 6.43031 11.93Z" fill="#8A8A8A" />
+                  </svg>
+                </button>
+                <input type="number" id="products" value="200">
+                <button onclick="changeValue('products', 1)">
+                  <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.5775 0.0699997C0.4675 0.12 0.3675 0.18 0.2675 0.27C0.1775 0.360001 0.1175 0.46 0.0675001 0.57C-0.0224999 0.79 -0.0224999 1.05 0.0675001 1.28C0.1175 1.39 0.1775 1.49 0.2675 1.58L4.7575 6L0.2775 10.42C0.1875 10.51 0.1275 10.61 0.0775 10.72C-0.0125 10.95 -0.0125 11.21 0.0775 11.43C0.1275 11.54 0.1975 11.64 0.2775 11.73C0.3675 11.81 0.4675 11.88 0.5875 11.93C0.6975 11.98 0.8175 12 0.9375 12C1.0575 12 1.1775 11.98 1.2875 11.93C1.3975 11.88 1.4975 11.82 1.5875 11.73L6.7375 6.65C6.8275 6.56 6.8875 6.46 6.9375 6.35C6.9875 6.24 7.0075 6.12 7.0075 6C7.0075 5.88 6.9875 5.76 6.9375 5.64C6.8875 5.53 6.8275 5.43 6.7375 5.34L1.5875 0.27C1.4975 0.190001 1.3975 0.12 1.2875 0.0699997C1.1775 0.0299997 1.0575 1.26106e-08 0.9375 1.11796e-08C0.8175 9.74859e-09 0.6975 0.0199995 0.5775 0.0699997Z" fill="#8A8A8A" />
+                  </svg>
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Ilość kolorów</td>
+            <td>
+              <div class="counter">
+                <button onclick="changeValue('colors', -1)">
+                  <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.43031 11.93C6.54031 11.88 6.64031 11.82 6.74031 11.73C6.83031 11.64 6.89031 11.54 6.94031 11.43C7.03031 11.21 7.03031 10.95 6.94031 10.72C6.89031 10.61 6.83031 10.51 6.74031 10.42L2.25031 6L6.73031 1.58C6.82031 1.49 6.88031 1.39 6.93031 1.28C7.02031 1.05 7.02031 0.79 6.93031 0.57C6.88031 0.46 6.81031 0.36 6.73031 0.27C6.64031 0.19 6.54031 0.12 6.42031 0.07C6.31031 0.02 6.19031 9.74859e-09 6.07031 1.11796e-08C5.95031 1.26106e-08 5.83031 0.02 5.72031 0.07C5.61031 0.12 5.51031 0.18 5.42031 0.27L0.270312 5.35C0.180312 5.44 0.120313 5.54 0.0703126 5.65C0.0203124 5.76 0.000312398 5.88 0.0003124 6C0.000312401 6.12 0.0203124 6.24 0.0703126 6.36C0.120313 6.47 0.180312 6.57 0.270312 6.66L5.42031 11.73C5.51031 11.81 5.61031 11.88 5.72031 11.93C5.83031 11.97 5.95031 12 6.07031 12C6.19031 12 6.31031 11.98 6.43031 11.93Z" fill="#8A8A8A" />
+                  </svg>
+                </button>
+                <input type="number" id="colors" value="1">
+                <button onclick="changeValue('colors', 1)">
+                  <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.5775 0.0699997C0.4675 0.12 0.3675 0.18 0.2675 0.27C0.1775 0.360001 0.1175 0.46 0.0675001 0.57C-0.0224999 0.79 -0.0224999 1.05 0.0675001 1.28C0.1175 1.39 0.1775 1.49 0.2675 1.58L4.7575 6L0.2775 10.42C0.1875 10.51 0.1275 10.61 0.0775 10.72C-0.0125 10.95 -0.0125 11.21 0.0775 11.43C0.1275 11.54 0.1975 11.64 0.2775 11.73C0.3675 11.81 0.4675 11.88 0.5875 11.93C0.6975 11.98 0.8175 12 0.9375 12C1.0575 12 1.1775 11.98 1.2875 11.93C1.3975 11.88 1.4975 11.82 1.5875 11.73L6.7375 6.65C6.8275 6.56 6.8875 6.46 6.9375 6.35C6.9875 6.24 7.0075 6.12 7.0075 6C7.0075 5.88 6.9875 5.76 6.9375 5.64C6.8875 5.53 6.8275 5.43 6.7375 5.34L1.5875 0.27C1.4975 0.190001 1.3975 0.12 1.2875 0.0699997C1.1775 0.0299997 1.0575 1.26106e-08 0.9375 1.11796e-08C0.8175 9.74859e-09 0.6975 0.0199995 0.5775 0.0699997Z" fill="#8A8A8A" />
+                  </svg>
+                </button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Pozycja nadruku</td>
+            <td>
+              <select name="branding-position" id="branding-position" class="form-select">
+                <option value="Przód">Przód</option>
+              </select>
+            </td>
+          </tr>
+        </table>
+        <div class="branding-select-text-content">
+            <strong>Podsumowanie:</strong>
+            <p>Cena za sztukę: 1,66 PLN x 200 sztuk</p>
+            <p>Znakowanie: 222 PLN</p>
+            <strong>Wartość całkowita netto zamówienia</strong>
+            <p class="green">100,66 <span>PLN</span></p>
+            <p>122.88 PLN brutto</p>
+            <button class="add-to-cart white">
+                <span>Do Koszyka</span>
+                <img src="./assets/icons/product_page/color-cart.svg" alt="koszyk" width="26" height="26">
+            </button>
+            <div class="delivery-text">
+                <img src="./assets/icons/product_page/delivery-car.svg" alt="dostawa" width="41" height="41">
+                <span>Przewidywiany czas realizacji: <strong class="green">do 3 dni</strong></span>
+            </div>
         </div>
-        <div class="options">
-            <div class="option" onclick="selectOption('Sitodruk')">Sitodruk</div>
-            <div class="option" onclick="selectOption('Druk cyfrowy')">Druk cyfrowy</div>
-            <div class="option" onclick="selectOption('Termotransfer')">Termotransfer</div>
-        </div>
+      </div>
     </div>
   </div>
 </div>
-
 <script>
-        function increase() {
-            let input = document.getElementById('price-value');
-            input.value = Math.max(0, parseInt(input.value) + 1);
-        }
+  function increase() {
+    let input = document.getElementById('price-value');
+    input.value = Math.max(0, parseInt(input.value) + 1);
+  }
 
-        function decrease() {
-            let input = document.getElementById('price-value');
-            input.value = Math.max(0, parseInt(input.value) - 1);
-        }
+  function decrease() {
+    let input = document.getElementById('price-value');
+    input.value = Math.max(0, parseInt(input.value) - 1);
+  }
 
-        function toggleDropdown() {
-            document.querySelector('.branding-custom-select').classList.toggle('open');
-        }
+  function toggleDropdown() {
+    document.querySelector('.branding-custom-select').classList.toggle('open');
+  }
+  document.addEventListener('click', function(event) {
+    let dropdown = document.querySelector('.branding-custom-select');
+    if (!dropdown.contains(event.target)) {
+      dropdown.classList.remove('open');
+    }
+  });
 
-        function selectOption(value) {
-            document.getElementById('selected-option').innerText = value;
-            toggleDropdown();
-        }
-
-        document.addEventListener('click', function(event) {
-            let dropdown = document.querySelector('.branding-custom-select');
-            if (!dropdown.contains(event.target)) {
-                dropdown.classList.remove('open');
-            }
-        });
+  function changeValue(id, delta) {
+    let input = document.getElementById(id);
+    let newValue = parseInt(input.value) + delta;
+    if (newValue >= 0) { // Prevent negative values
+      input.value = newValue;
+    }
+  }
 </script>
