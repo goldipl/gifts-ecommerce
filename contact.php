42ee1526158885e1e8091df8c30e170dd2730ed5
goldipl <mgodlewski92@gmail.com>
2025-02-20 07:21:37 +0100

    delete unused faq files

--- contact.php
+++ contact.php
@@ -31,9 +31,6 @@
         <footer> 
             <?php include "./components/common/footer.php"; ?> 
         </footer>
-        <script>
-            document.querySelector('#topbar .container .topbar-links a:nth-of-type(6)').classList.add('active')
-        </script>
         <script src="./js/jquery.min.js"></script>
         <script src="./js/popper.min.js"></script>
         <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
