98c5ba174664371ec57e6f1dfa7cf553f5770bef
goldipl <mgodlewski92@gmail.com>
2025-02-24 18:11:11 +0100

    fill and styling cart thank you page

---
+++ components/cart/cart_thank_you_page_content.php
@@ -0,0 +1,19 @@
+<div class="thankyou-box">
+    <div class="thankyou-box__top">
+        <img src="./assets/icons/cart/green-checked.svg" width="59" height="59" alt="gotowe" class="checked-img">
+        <h2 class="thankyou-box__title">Dziękujemy</h2>
+        <p class="thankyou-box__description">Płatność przebiegła pomyślnie</p>
+    </div>
+    <div class="thankyou-box__bottom">
+        <p class="thankyou-box__description">Przyjęliśmy Twoje zamówienia do realizacji.</p>
+        <p class="thankyou-box__description">Numer twojego zamówienia to: <strong class="green">3289</strong></p>
+    </div>
+</div>
+
+<div class="bottom-cart-box-with-btn">
+          <div class="bottom-cart-buttons">
+            <a href="#" class="white-btn prev">
+              <span>Do strony głównej</span>
+            </a>
+          </div>
+        </div>
