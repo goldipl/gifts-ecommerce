ecea156ba36038ff05e640011392237ecdd7e926
goldipl <mgodlewski92@gmail.com>
2025-02-24 19:05:54 +0100

    fill & styling dashboard menu

--- components/dashboard/dashboard_menu.php
+++ components/dashboard/dashboard_menu.php
@@ -1,46 +1,39 @@
 <aside class="dashboard-menu">
-    <div class="header">
-        <p>Menu</p>
-    </div>
     <div class="expand-menu">
         <p><img src="./assets/icons/common/grey-arrow-down.svg" alt="arrow down icon" width="12" height="8">Rozwiń menu<img src="./assets/icons/common/grey-arrow-down.svg" alt="arrow down icon" width="12" height="8"></p>
     </div>
     <div class="menu-links-container">
-        <div class="menu-links-box big-profile">
-            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/profile.svg" alt="menu icon" width="20" height="20">Profil</a>
-            <a class="menu-link" href="./dashboard-your-data.php">Twoje dane</a>
-            <a class="menu-link" href="./dashboard-newsletter.php">Newsletter</a>
-            <a class="menu-link" href="./dashboard-program.php">Program Rozwoju</a>
+        <div class="menu-hello-box">
+            <span>Witaj!</span>
+            <strong>Jan Kowalski</strong>
         </div>
-        <div class="menu-links-box big-orders">
-            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/orders.svg" alt="menu icon" width="20" height="20">Zamówienia</a>
-            <a class="menu-link" href="./dashboard-orders.php">Zamówienia</a>
-            <a class="menu-link" href="./dashboard-fv-delivery-letters.php">FV i listy przewozowe</a>
-            <a class="menu-link" href="./dashboard-delivery-addresses.php">Adresy dostaw</a>
-            <a class="menu-link" href="./dashboard-visualizations.php">Wizualizacje</a>
-            <a class="menu-link" href="./dashboard-logos.php">Logotypy</a>
+        <div class="menu-links-box">
+            <a class="menu-link" href="">
+                <img src="./assets/icons/dashboard/desktop.svg" alt="ikona" class="menu-link__img">
+                <span class="menu-link__text">Pulpit</span>
+            </a>
+            <a class="menu-link" href="">
+                <img src="./assets/icons/dashboard/orders.svg" alt="ikona" class="menu-link__img">
+                <span class="menu-link__text">Zamówienia</span>
+            </a>
+            <a class="menu-link" href="">
+                <img src="./assets/icons/dashboard/your-data.svg" alt="ikona" class="menu-link__img">
+                <span class="menu-link__text">Twoje Dane</span>
+            </a>
+            <a class="menu-link" href="">
+                <img src="./assets/icons/dashboard/invoice-data.svg" alt="ikona" class="menu-link__img">
+                <span class="menu-link__text">Dane do faktury</span>
+            </a>
+            <a class="menu-link" href="">
+                <img src="./assets/icons/dashboard/delivery-address.svg" alt="ikona" class="menu-link__img">
+                <span class="menu-link__text">Adres wysyłki</span>
+            </a>
+            <button class="dashboard-logout-btn">
+            <svg width="27" height="15" viewBox="0 0 27 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.797883 8.24656C0.798188 8.24688 0.798441 8.24725 0.798798 8.24756L6.10564 13.7319C6.50321 14.1428 7.14625 14.1413 7.54199 13.7284C7.93768 13.3155 7.93615 12.6477 7.53859 12.2368L3.97557 8.55469L25.4844 8.55469C26.0453 8.55469 26.5 8.08251 26.5 7.5C26.5 6.9175 26.0453 6.44532 25.4844 6.44532L3.97562 6.44531L7.53854 2.76319C7.9361 2.35228 7.93763 1.68451 7.54194 1.27165C7.1462 0.858688 6.50311 0.857265 6.10559 1.26806L0.798747 6.75244C0.798442 6.75276 0.798188 6.75312 0.797833 6.75344C0.400063 7.16572 0.401331 7.83566 0.797883 8.24656Z" fill="#253037"/>
+            </svg>
+            <span>Wyloguj się</span>
+            </button>
         </div>
-        <div class="menu-links-box big-offerts">
-            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/offerts.svg" alt="menu icon" width="20" height="20">Oferty</a>
-            <a class="menu-link" href="./dashboard-commercial-offers.php">Oferty handlowe</a>
-            <a class="menu-link" href="./dashboard-your-offers.php">Twój ofertownik</a>
-            <a class="menu-link" href="./dashboard-calculations.php">Kalkulacje</a>
-            <a class="menu-link" href="./dashboard-negotiations.php">Negocjacje</a>
-        </div>
-        <div class="menu-links-box big-to-download">
-            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/to_download.svg" alt="menu icon" width="20" height="20">Do pobrania</a>
-            <a class="menu-link" href="./dashboard-pricelists.php">Cenniki</a>
-            <a class="menu-link" href="./dashboard-catalogs.php">Katalogi</a>
-            <a class="menu-link" href="./dashboard-newsletters.php">Newslettery</a>
-            <a class="menu-link" href="./dashboard-videos.php">Video</a>
-            <a class="menu-link" href="./dashboard-marketing-materials.php">Materiały marketingowe</a>
-        </div>
-        <div class="menu-links-box big-web-api">
-            <a class="menu-link-big" href="./dashboard-webapi.php"><img src="./assets/icons/dashboard/api.svg" alt="menu icon" width="20" height="20">Web API</a>
-        </div>
-        <div class="menu-links-box big-no-name-shop">
-            <a class="menu-link-big" href="#"><img src="./assets/icons/dashboard/cart.svg" alt="menu icon" width="20" height="20">Sklep No-Name</a>
-        </div>
-        <button class="logout">Wyloguj się</button>
     </div>
 </aside>
--- components/dashboard/dashboard_title.php
+++
@@ -1,5 +0,0 @@
-<aside class="dashboard-main-title">
-    <div class="header">
-        <p>Witaj w Panelu Klienta</p>
-    </div>
-</aside>
