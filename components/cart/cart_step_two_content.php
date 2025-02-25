70bc9ab8f82685c83581928e740fed130c14bf74
goldipl <mgodlewski92@gmail.com>
2025-02-25 19:48:23 +0100

    beautify code

--- components/cart/cart_step_two_content.php
+++ components/cart/cart_step_two_content.php
@@ -10,7 +10,6 @@
             <span class="custom-radio-checkmark"></span>
           </label>
         </div>
-
         <div class="address-inputs-wrapper step-two-bill-wrapper">
           <div class="input-slot">
             <label for="select-country-delivery">Wybierz kraj</label>
@@ -20,101 +19,98 @@
             </select>
           </div>
           <div class="input-slot">
-              <label for="name">Imię *</label>
-              <input class="form-input" type="text" name="name" id="name" placeholder="np. Krzysztof" required>
+            <label for="name">Imię *</label>
+            <input class="form-input" type="text" name="name" id="name" placeholder="np. Krzysztof" required>
+          </div>
+          <div class="input-slot">
+            <label for="surname">Nazwisko *</label>
+            <input class="form-input" type="text" name="surname" id="surname" placeholder="np. Nowak" required>
+          </div>
+          <div class="inputs-row">
+            <div class="input-slot">
+              <label for="street">Ulica *</label>
+              <input class="form-input" type="text" name="street" id="street" placeholder="np. Warszawska" required>
             </div>
             <div class="input-slot">
-              <label for="surname">Nazwisko *</label>
-              <input class="form-input" type="text" name="surname" id="surname" placeholder="np. Nowak" required>
-            </div>
-            <div class="inputs-row">
-              <div class="input-slot">
-                <label for="street">Ulica *</label>
-                <input class="form-input" type="text" name="street" id="street" placeholder="np. Warszawska" required>
-              </div>
-              <div class="input-slot">
-                <label for="home-number">Nr domu *</label>
-                <input class="form-input" type="text" name="home-number" id="home-number" placeholder="np. 320" required>
-              </div>
-              <div class="input-slot">
-                <label for="apartment-number">Nr lokalu</label>
-                <input class="form-input" type="text" name="apartment-number" id="apartment-number" placeholder="np. 15">
-              </div>
-            </div>
-            <div class="inputs-row">
-              <div class="input-slot">
-                <label for="zip-code">Kod pocztowy *</label>
-                <input class="form-input" type="text" name="zip-code" id="zip-code" placeholder="00-030" required>
-              </div>
-              <div class="input-slot">
-                <label for="city">Miejscowość *</label>
-                <input class="form-input" type="text" name="city" id="city" placeholder="np. Warszawa" required>
-              </div>
+              <label for="home-number">Nr domu *</label>
+              <input class="form-input" type="text" name="home-number" id="home-number" placeholder="np. 320" required>
             </div>
             <div class="input-slot">
-              <label for="telephone">Telefon</label>
-              <input class="form-input" type="text" name="telephone" id="telephone" placeholder="np. 555 555 555">
+              <label for="apartment-number">Nr lokalu</label>
+              <input class="form-input" type="text" name="apartment-number" id="apartment-number" placeholder="np. 15">
+            </div>
+          </div>
+          <div class="inputs-row">
+            <div class="input-slot">
+              <label for="zip-code">Kod pocztowy *</label>
+              <input class="form-input" type="text" name="zip-code" id="zip-code" placeholder="00-030" required>
             </div>
             <div class="input-slot">
-              <label for="email">E-mail *</label>
-              <input class="form-input" type="email" name="email" id="email" placeholder="Email lub login" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+              <label for="city">Miejscowość *</label>
+              <input class="form-input" type="text" name="city" id="city" placeholder="np. Warszawa" required>
             </div>
-            <div class="input-slot">
-                <label for="comments">Uwagi</label>
-                <textarea class="form-textarea" name="comments" id="comments" placeholder="Tutaj możesz wpisać swoje uwagi do zamówienia"></textarea>
-            </div>
+          </div>
+          <div class="input-slot">
+            <label for="telephone">Telefon</label>
+            <input class="form-input" type="text" name="telephone" id="telephone" placeholder="np. 555 555 555">
+          </div>
+          <div class="input-slot">
+            <label for="email">E-mail *</label>
+            <input class="form-input" type="email" name="email" id="email" placeholder="Email lub login" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+          </div>
+          <div class="input-slot">
+            <label for="comments">Uwagi</label>
+            <textarea class="form-textarea" name="comments" id="comments" placeholder="Tutaj możesz wpisać swoje uwagi do zamówienia"></textarea>
+          </div>
         </div>
-
         <div class="other-address-checkbox-wrapper">
           <label class="custom-checkbox-container other-address-checkbox">
             <div class="register-checkbox"></div>Dostawa na inny adres <input type="checkbox" class="custom_input">
             <span class="custom-checkbox-checkmark"></span>
           </label>
         </div>
-
         <div class="address-inputs-wrapper step-two-bill-wrapper__other-address-delivery">
           <div class="input-slot">
-              <label for="name-other-address">Imię *</label>
-              <input class="form-input" type="text" name="name-other-address" id="name-other-address" placeholder="np. Krzysztof" required>
+            <label for="name-other-address">Imię *</label>
+            <input class="form-input" type="text" name="name-other-address" id="name-other-address" placeholder="np. Krzysztof" required>
+          </div>
+          <div class="input-slot">
+            <label for="surname-other-address">Nazwisko *</label>
+            <input class="form-input" type="text" name="surname-other-address" id="surname-other-address" placeholder="np. Nowak" required>
+          </div>
+          <div class="inputs-row">
+            <div class="input-slot">
+              <label for="street-other-address">Ulica *</label>
+              <input class="form-input" type="text" name="street-other-address" id="street-other-address" placeholder="np. Warszawska" required>
             </div>
             <div class="input-slot">
-              <label for="surname-other-address">Nazwisko *</label>
-              <input class="form-input" type="text" name="surname-other-address" id="surname-other-address" placeholder="np. Nowak" required>
-            </div>
-            <div class="inputs-row">
-              <div class="input-slot">
-                <label for="street-other-address">Ulica *</label>
-                <input class="form-input" type="text" name="street-other-address" id="street-other-address" placeholder="np. Warszawska" required>
-              </div>
-              <div class="input-slot">
-                <label for="home-number-other-address">Nr domu *</label>
-                <input class="form-input" type="text" name="home-number-other-address" id="home-number-other-address" placeholder="np. 320" required>
-              </div>
-              <div class="input-slot">
-                <label for="apartment-number-other-address">Nr lokalu</label>
-                <input class="form-input" type="text" name="apartment-number-other-address" id="apartment-number-other-address" placeholder="np. 15">
-              </div>
-            </div>
-            <div class="inputs-row">
-              <div class="input-slot">
-                <label for="zip-code-other-address">Kod pocztowy *</label>
-                <input class="form-input" type="text" name="zip-code-other-address" id="zip-code-other-address" placeholder="00-030" required>
-              </div>
-              <div class="input-slot">
-                <label for="city-other-address">Miejscowość *</label>
-                <input class="form-input" type="text" name="city-other-address" id="city-other-address" placeholder="np. Warszawa" required>
-              </div>
+              <label for="home-number-other-address">Nr domu *</label>
+              <input class="form-input" type="text" name="home-number-other-address" id="home-number-other-address" placeholder="np. 320" required>
             </div>
             <div class="input-slot">
-              <label for="telephone-other-address">Telefon</label>
-              <input class="form-input" type="text" name="telephone-other-address" id="telephone-other-address" placeholder="np. 555 555 555">
+              <label for="apartment-number-other-address">Nr lokalu</label>
+              <input class="form-input" type="text" name="apartment-number-other-address" id="apartment-number-other-address" placeholder="np. 15">
+            </div>
+          </div>
+          <div class="inputs-row">
+            <div class="input-slot">
+              <label for="zip-code-other-address">Kod pocztowy *</label>
+              <input class="form-input" type="text" name="zip-code-other-address" id="zip-code-other-address" placeholder="00-030" required>
             </div>
             <div class="input-slot">
-              <label for="email-other-address">E-mail *</label>
-              <input class="form-input" type="email" name="email-other-address" id="email-other-address" placeholder="Email lub login" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+              <label for="city-other-address">Miejscowość *</label>
+              <input class="form-input" type="text" name="city-other-address" id="city-other-address" placeholder="np. Warszawa" required>
             </div>
+          </div>
+          <div class="input-slot">
+            <label for="telephone-other-address">Telefon</label>
+            <input class="form-input" type="text" name="telephone-other-address" id="telephone-other-address" placeholder="np. 555 555 555">
+          </div>
+          <div class="input-slot">
+            <label for="email-other-address">E-mail *</label>
+            <input class="form-input" type="email" name="email-other-address" id="email-other-address" placeholder="Email lub login" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+          </div>
         </div>
-
       </div>
       <div class="bottom-cart-box-with-btn">
         <div class="bottom-cart-buttons">
@@ -149,23 +145,19 @@
     </aside>
   </div>
 </div>
-
 <script>
-  document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", function() {
     const checkbox = document.querySelector(".other-address-checkbox input[type='checkbox']");
     const addressWrapper = document.querySelector(".address-inputs-wrapper.step-two-bill-wrapper__other-address-delivery");
 
     function toggleAddressWrapper() {
-        if (checkbox.checked) {
-            addressWrapper.style.display = "block";
-        } else {
-            addressWrapper.style.display = "none";
-        }
+      if (checkbox.checked) {
+        addressWrapper.style.display = "block";
+      } else {
+        addressWrapper.style.display = "none";
+      }
     }
-
     checkbox.addEventListener("change", toggleAddressWrapper);
-
     toggleAddressWrapper();
-});
-
+  });
 </script>
