0e35a425125255db420cde6acac553b226d8e5c4
goldipl <mgodlewski92@gmail.com>
2025-02-25 18:24:37 +0100

    fill and styling dashboard layout, first screen

--- components/dashboard/dashboard_main_page.php
+++ components/dashboard/dashboard_main_page.php
@@ -1,3 +1,77 @@
 <div class="dashboard-main">
-
+  <h2>Ostatnie zamówienia:</h2>
+  <div class="dashboard-wrapper">
+    <div class="dashboard-table-container">
+      <table class="dashboard-table">
+        <tbody>
+          <tr>
+            <th>Zamówienie</th>
+            <th>Data</th>
+            <th>Dostawa</th>
+            <th>Wartość</th>
+            <th>Status</th>
+            <th>Akcje </th>
+          </tr>
+          <tr>
+            <td>007347914</td>
+            <td>27.12.2024</td>
+            <td>Jan Kowalski</td>
+            <td>9,49 PLN</td>
+            <td class="green">Odebrane</td>
+            <td>
+              <a href="">Szczegóły zamówienia</a>
+            </td>
+          </tr>
+          <tr>
+            <td>007347914</td>
+            <td>27.12.2024</td>
+            <td>Jan Kowalski</td>
+            <td>9,49 PLN</td>
+            <td class="red">Odebrane</td>
+            <td>
+              <a href="">Szczegóły zamówienia</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="dashboard-data-container">
+      <div class="dashboard-data">
+        <h2>Dane konta:</h2>
+        <div class="data">
+          <p>Jan Kowalski</p>
+          <p>jan.kowalski@gmail.com</p>
+        </div>
+        <div class="buttons">
+          <a href="" class="white-btn">Edytuj dane</a>
+          <a href="" class="white-btn">Zmień hasło</a>
+        </div>
+      </div>
+      <div class="dashboard-address">
+        <h2>Moje adresy:</h2>
+        <div class="addresses-dashboard-box">
+          <div class="data">
+            <strong>Domyślny adres rozliczeniowy:</strong>
+            <p>Jan Kowalski</p>
+            <p>Warszawska 320</p>
+            <p>00-320 Warszawa</p>
+            <p>tel. 555 555 555</p>
+            <div class="buttons">
+              <a href="" class="white-btn">Edytuj adres</a>
+            </div>
+          </div>
+          <div class="data">
+            <strong>Domyślny adres dostawy:</strong>
+            <p>Jan Kowalski</p>
+            <p>Warszawska 320</p>
+            <p>00-320 Warszawa</p>
+            <p>tel. 555 555 555</p>
+            <div class="buttons">
+              <a href="" class="white-btn">Edytuj adres</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
